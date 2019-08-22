251442a28f9a148649e03c240232942ae4ec89b2
mitaskq <mitaskq@mi-hp-tk>
2019-08-23 00:01:56 +0300

    new commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Javalin demo application
+
+Application that use Javalin framework to develop REST services, each of that providing an HTML response, in the form of simple pages.
---
+++ src/javalin/test/Test1.java
@@ -0,0 +1,28 @@
+package javalin.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import io.javalin.Javalin;
+
+public class Test1 {
+
+	public final static int PORT = 8089;
+
+	public static String getContentPage(boolean withTime) {
+		String htmlPage = "<!DOCTYPE> <html><head><title>Message with timestamp</title></head><body>";
+
+		htmlPage += "<h2>" + (withTime ? LocalDateTime.now() : LocalDate.now()) + "</h2>";
+		htmlPage += "</body></html>";
+		
+		return htmlPage;
+	}
+
+	public static void main(String[] args) {
+
+		Javalin app = Javalin.create().start(PORT);
+
+		app.get("/date", ctx -> ctx.html(getContentPage(false)));
+		app.get("/datetime", ctx -> ctx.html(getContentPage(true)));
+	}
+}
